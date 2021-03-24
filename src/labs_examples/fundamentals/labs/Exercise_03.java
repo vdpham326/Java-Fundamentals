5e740040226a8d06c05c1b45db5ef6445175797e
Vu Pham <vdpham326@gmail.com>
2021-03-24 08:44:00 -0700

    completed exercise 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -2,21 +2,26 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 3: My First Program
- *
- *      Write the necessary code to display the follow message to the console
- *
- *      Hello World!
- *      Check out my first program!
- *
+ * <p>
+ * Write the necessary code to display the follow message to the console
+ * <p>
+ * Hello World!
+ * Check out my first program!
  */
 
 public class Exercise_03 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
+        System.out.println("Hello World");
+        System.out.println("Check out my first program");
+        int i = 10;
+        int x = 20;
+        int z = i + x;
+
+        System.out.println(z);
 
     }
 
